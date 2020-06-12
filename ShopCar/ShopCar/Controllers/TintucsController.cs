559482bb08c55fd3607f56cb7ecf227897216e48
Duy Vu <duyvu1999pro@gmail.com>
2020-06-12 23:06:37 +0700

    update admin

---
+++ ShopCar/ShopCar/Controllers/TintucsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ShopCar.Model;
+
+namespace ShopCar.Controllers
+{
+    public class TintucsController : Controller
+    {
+        private CarShopEntities db = new CarShopEntities();
+
+        // GET: Tintucs
+        public ActionResult Index()
+        {
+            return View(db.Tintucs.ToList());
+        }
+
+        // GET: Tintucs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tintuc tintuc = db.Tintucs.Find(id);
+            if (tintuc == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tintuc);
+        }
+
+        // GET: Tintucs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tintucs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaTin,URLAnh,NoiDung")] Tintuc tintuc)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tintucs.Add(tintuc);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tintuc);
+        }
+
+        // GET: Tintucs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tintuc tintuc = db.Tintucs.Find(id);
+            if (tintuc == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tintuc);
+        }
+
+        // POST: Tintucs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaTin,URLAnh,NoiDung")] Tintuc tintuc)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tintuc).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tintuc);
+        }
+
+        // GET: Tintucs/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tintuc tintuc = db.Tintucs.Find(id);
+            if (tintuc == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tintuc);
+        }
+
+        // POST: Tintucs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Tintuc tintuc = db.Tintucs.Find(id);
+            db.Tintucs.Remove(tintuc);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
